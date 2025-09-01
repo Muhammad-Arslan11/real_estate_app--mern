a406f4fd706051539abce56b33a64284b9965c66
Muhammad Arslan Shah <muhammad.arslan9799@gmail.com>
2025-09-01 17:09:53 +0500

    add dynamic pages

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import {FaSearch} from 'react-icons';
+import {FaSearch} from 'react-icons/fa';
+import Home from '../pages/Home.jsx';
+import About from '../pages/About.jsx';
 
 function Header() {
     const [searchTerm, setSearchTerm] = useState('');
-    c
+
+    const handleSubmit = ()=>{
+
+    }
+    const currentUser = false;
   return (
     <header className='bg-slate-200 shadow-md'>
            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -32,19 +38,19 @@ function Header() {
           <ul className='flex gap-4'>
           <Link to='/'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>
-              Home
+              <Home/>
             </li>
           </Link>
           <Link to='/about'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>
-              About
+              <About/>
             </li>
           </Link>
           <Link to='/profile'>
             {currentUser ? (
               <img
                 className='rounded-full h-7 w-7 object-cover'
-                src={currentUser.avatar}
+                src={currentUser?.avatar}
                 alt='profile'
               />
             ) : (
