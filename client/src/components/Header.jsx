79e3d845ce98460b9ee5c18c38196b2afabdcb88
Muhammad Arslan Shah <muhammad.arslan9799@gmail.com>
2025-09-04 18:27:25 +0500

    some issues fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -46,17 +46,20 @@ function Header() {
               <About/>
             </li>
           </Link>
-          <Link to='/profile'>
             {currentUser ? (
+          <Link to='/profile'>
+
               <img
                 className='rounded-full h-7 w-7 object-cover'
                 src={currentUser?.avatar}
                 alt='profile'
               />
-            ) : (
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
-            )}
           </Link>
+            ) : (
+              <Link  to='/sign-in'>
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+              </Link>
+            )}
         </ul>
         </div>
     </header>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store , persistor} from "./store/store.js";
-import {PersistGate} from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
