d2e6af11f515af9c0f4c11be730efaef2bca9d63
Muhammad Arslan Shah <muhammad.arslan9799@gmail.com>
2025-09-04 14:55:14 +0500

    fix the name of variable

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,22 +1,22 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import userReducer from './userSlice';
-import {persistReducer, presistStore} from 'redux-persist';
-import {storage} from '@redux-persist/lib/storage';
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({user: userReducer});
 const persistConfig = {
     key: 'root',
     storage,
-    verison: 1,
+    version: 1,
 }
 
-const persistReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: persistReducer,
+    reducer: persistedReducer,
     middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
       serializableCheck: false,
     })
 });
 
-export const persistor = presistStore(store);
+export const persistor = persistStore(store);
