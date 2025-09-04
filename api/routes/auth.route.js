5d3be1dfafbda94f48db052fe62366d0b3208960
Muhammad Arslan Shah <muhammad.arslan9799@gmail.com>
2025-09-04 19:26:42 +0500

    add google auth route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/sign-up',signUp);
 router.post('/sign-in',signIn);
+router.post('/google');
 
 export default router;
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import {GoogleAuthProvider, signInWithPopup, getAuth} from 'firebase/auth';
+import {app} from '../firebase/firebase';
+import { signInSuccess } from '../store/userSlice';
 
 function OAuth() {
+
+  const handleGoogleClick = async ()=>{
+    try {
+       const provider = new GoogleAuthProvider();
+    const auth = getAuth(app);
+    const result = await signInWithPopup(auth, provider);
+
+     const res = await fetch("/api/auth/google", {
+            method: 'POST',
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify({name:result.user.displayName, email:result.user.email, photo:result.user.photoURL}),
+          });
+          const data = await res.json();
+          dispatch(signInSuccess(data));
+          navigate('/dashboard');
+    
+    } catch (error) {
+      console.log("error from Oauth: ",error);
+    }
+   
+  }
   return (
-    <div>OAuth</div>
+     <button
+      onClick={handleGoogleClick}
+      type='button'
+      className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+    >
+      Continue with google
+    </button>
   )
 }
 
